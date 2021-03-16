bed57233d90f021d3c9b17672e536a8fc6b89538
Vitalii <tka4uk.gt41@gmail.com>
2021-03-17 00:06:44 +0600

    adaptive slider

--- js/main.js
+++ js/main.js
@@ -1,16 +1,25 @@
-$(function(){
-    $('.reviews-slider').slick({
-        // setting-name: setting-value
-        infinite: true,
-        slidesToShow: 2,
-        slidesToScroll: 2,
-        dots: true,
-        arrows: false,
-        autoplay: true,
-        autoplaySpeed: 1000
-      });
+$(function () {
+  $('.reviews-slider').slick({
+    // setting-name: setting-value
+    infinite: true,
+    slidesToShow: 2,
+    slidesToScroll: 2,
+    dots: true,
+    arrows: false,
+    autoplay: false,
+    autoplaySpeed: 1000,
+    responsive: [
+      {
+        breakpoint: 1106,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    ]
+  });
 
-      $('.btn__menu').on('click', function(){
-        $('.menu__list').slideToggle();
-      });
+  $('.btn__menu').on('click', function () {
+    $('.menu__list').slideToggle();
+  });
 });
